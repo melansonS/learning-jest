e376f6e32cba1916d8e01a5578d16cb24b007e0d
Samuel Melanson <samuelmelanson@live.com>
2022-03-21 16:34:49 -0400

    next router navigation between tabs using url query params

--- react-tabs/components/Tabs/Tabs.js
+++ react-tabs/components/Tabs/Tabs.js
@@ -1,30 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./tabs.module.css";
+import { useRouter } from "next/router";
+import { slugify } from "../../utils/slugify";
 
-const Tabs = ({ children }) => {
+const Tabs = ({ children, initialTab }) => {
   const [activeTab, setActiveTab] = useState(children[0].props.label);
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(`${router.pathname}?path=${slugify(activeTab)}`, undefined, {
+      shallow: true,
+    });
+  }, [activeTab]);
+
+  useEffect(() => {
+    if (initialTab) {
+      setActiveTab(initialTab);
+    }
+  }, []);
 
   const handleClick = (e, newActiveTab) => {
     e.preventDefault();
-    setActiveTab(newActiveTab);
+    setActiveTab(slugify(newActiveTab));
   };
 
   return (
     <div>
       <ul className={styles.tabs}>
-        {children.map((tab) => (
-          <li
-            className={tab.props.label === activeTab ? styles.current : ""}
-            key={tab.props.label}
-          >
-            <a href="#" onClick={(e) => handleClick(e, tab.props.label)}>
-              {tab.props.label}
-            </a>
-          </li>
-        ))}
+        {children.map((tab) => {
+          const label = tab.props.label;
+          return (
+            <li
+              className={slugify(label) === activeTab ? styles.current : ""}
+              key={label}
+            >
+              <a href="#" onClick={(e) => handleClick(e, label)}>
+                {label}
+              </a>
+            </li>
+          );
+        })}
       </ul>
       {children.map((one) => {
-        if (one.props.label === activeTab) {
+        if (slugify(one.props.label) === activeTab) {
           return (
             <div className={styles.content} key={one.props.label}>
               {one.props.children}
--- react-tabs/pages/index.js
+++ react-tabs/pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-
+import { useTouter } from "next/router";
 import { Tabs } from "../components/Tabs";
 
-export default function Home() {
+export default function Home({ query }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,39 +11,39 @@ export default function Home() {
       </Head>
 
       <main>
-        <Tabs >
+        <Tabs initialTab={query.path}>
           <div label="Tab 1">
             <h2>Tab 1</h2>
             <p>
-              There are many variations of passages of Lorem Ipsum available, but
-              the majority have suffered alteration in some form, by injected
-              humour, or randomised words which don't look even slightly
-              believable. If you are going to use a passage of Lorem Ipsum, you
-              need to be sure there isn't anything embarrassing hidden in the
-              middle of text. All the Lorem Ipsum generators on the Internet tend
-              to repeat predefined chunks as necessary, making this the first true
-              generator on the Internet. It uses a dictionary of over 200 Latin
-              words, combined with a handful of model sentence structures, to
-              generate Lorem Ipsum which looks reasonable. The generated Lorem
-              Ipsum is therefore always free from repetition, injected humour, or
-              non-characteristic words etc.
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by
+              injected humour, or randomised words which don't look even
+              slightly believable. If you are going to use a passage of Lorem
+              Ipsum, you need to be sure there isn't anything embarrassing
+              hidden in the middle of text. All the Lorem Ipsum generators on
+              the Internet tend to repeat predefined chunks as necessary, making
+              this the first true generator on the Internet. It uses a
+              dictionary of over 200 Latin words, combined with a handful of
+              model sentence structures, to generate Lorem Ipsum which looks
+              reasonable. The generated Lorem Ipsum is therefore always free
+              from repetition, injected humour, or non-characteristic words etc.
             </p>
           </div>
           <div label="Tab 2">
             <h2>Tab 2</h2>
             <p>
-              sure there isn't anything embarrassing hidden in the middle of text.
-              All the Lorem Ipsum generators on the Internet tend to repeat
-              predefined chunks as necessary, making this the first true generator
-              on the Internet. It uses a dicti There are many variations of
-              passages of Lorem Ipsum available, but the majority have suffered
-              alteration in some form, by injected humour, or randomised words
-              which don't look even slightly believable. If you are going to use a
-              passage of Lorem Ipsum, you need to beonary of over 200 Latin words,
-              combined with a handful of model sentence structures, to generate
-              Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is
-              therefore always free from repetition, injected humour, or
-              non-characteristic words etc.
+              sure there isn't anything embarrassing hidden in the middle of
+              text. All the Lorem Ipsum generators on the Internet tend to
+              repeat predefined chunks as necessary, making this the first true
+              generator on the Internet. It uses a dicti There are many
+              variations of passages of Lorem Ipsum available, but the majority
+              have suffered alteration in some form, by injected humour, or
+              randomised words which don't look even slightly believable. If you
+              are going to use a passage of Lorem Ipsum, you need to beonary of
+              over 200 Latin words, combined with a handful of model sentence
+              structures, to generate Lorem Ipsum which looks reasonable. The
+              generated Lorem Ipsum is therefore always free from repetition,
+              injected humour, or non-characteristic words etc.
             </p>
           </div>
           <div label="Tab 3">
@@ -51,12 +51,13 @@ export default function Home() {
             <p>
               embarrassing hidden in the middle of text. All the Lorem Ipsum
               generators on the Internet tend to repeat predefined chunks as
-              necessary, making this the first true generator on the Internet. It
-              uses a dictionary of over 200 Latin words, combined with ich don't
-              look even slightly believable. If you are going to use a passage of
-              Lorem Ipsum, you need to be sure there isn't anythingh looks
-              reasonable. The generated Lorem Ipsum is therefore always free from
-              repetition, injected humour, or non-characteristic words etc.
+              necessary, making this the first true generator on the Internet.
+              It uses a dictionary of over 200 Latin words, combined with ich
+              don't look even slightly believable. If you are going to use a
+              passage of Lorem Ipsum, you need to be sure there isn't anythingh
+              looks reasonable. The generated Lorem Ipsum is therefore always
+              free from repetition, injected humour, or non-characteristic words
+              etc.
             </p>
           </div>
         </Tabs>
@@ -64,3 +65,7 @@ export default function Home() {
     </div>
   );
 }
+
+Home.getInitialProps = ({ query }) => {
+  return { query };
+};
