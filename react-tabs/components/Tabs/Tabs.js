da17f9a9fa1377017256277330ca0a370e7ce4ad
Samuel Melanson <samuelmelanson@live.com>
2022-03-21 16:14:58 -0400

    tab basic functionality

--- react-tabs/components/Tabs/Tabs.js
+++ react-tabs/components/Tabs/Tabs.js
@@ -1,63 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./tabs.module.css";
 
-const Tabs = () => {
+const Tabs = ({ children }) => {
+  const [activeTab, setActiveTab] = useState(children[0].props.label);
+
+  const handleClick = (e, newActiveTab) => {
+    e.preventDefault();
+    setActiveTab(newActiveTab);
+  };
+
   return (
     <div>
       <ul className={styles.tabs}>
-        <li className={styles.current}>Tab 1</li>
-        <li>Tab 2</li>
-        <li>Tab 3</li>
+        {children.map((tab) => (
+          <li
+            className={tab.props.label === activeTab ? styles.current : ""}
+            key={tab.props.label}
+          >
+            <a href="#" onClick={(e) => handleClick(e, tab.props.label)}>
+              {tab.props.label}
+            </a>
+          </li>
+        ))}
       </ul>
-      <div className={styles.content}>
-        <div>
-          <h2>Tab 1</h2>
-          <p>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </p>
-        </div>
-        <div>
-          <h2>Tab 2</h2>
-          <p>
-            sure there isn't anything embarrassing hidden in the middle of text.
-            All the Lorem Ipsum generators on the Internet tend to repeat
-            predefined chunks as necessary, making this the first true generator
-            on the Internet. It uses a dicti There are many variations of
-            passages of Lorem Ipsum available, but the majority have suffered
-            alteration in some form, by injected humour, or randomised words
-            which don't look even slightly believable. If you are going to use a
-            passage of Lorem Ipsum, you need to beonary of over 200 Latin words,
-            combined with a handful of model sentence structures, to generate
-            Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is
-            therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
-          </p>
-        </div>
-        <div>
-          <h2>Tab 3</h2>
-          <p>
-            embarrassing hidden in the middle of text. All the Lorem Ipsum
-            generators on the Internet tend to repeat predefined chunks as
-            necessary, making this the first true generator on the Internet. It
-            uses a dictionary of over 200 Latin words, combined with ich don't
-            look even slightly believable. If you are going to use a passage of
-            Lorem Ipsum, you need to be sure there isn't anythingh looks
-            reasonable. The generated Lorem Ipsum is therefore always free from
-            repetition, injected humour, or non-characteristic words etc.
-          </p>
-        </div>
-      </div>
+      {children.map((one) => {
+        if (one.props.label === activeTab) {
+          return (
+            <div className={styles.content} key={one.props.label}>
+              {one.props.children}
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
